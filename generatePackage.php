a9e63e23a884d515079dfb82f926756f2402fcb5
Bill Shupp <hostmaster@shupp.org>
2011-02-15 12:10:03 -0800

    PEARified the project

---
+++ generatePackage.php
@@ -0,0 +1,57 @@
+<?php
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+require_once('PEAR/PackageFileManager2.php');
+
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$packagexml = new PEAR_PackageFileManager2;
+
+$packagexml->setOptions(array(
+    'baseinstalldir'    => '/',
+    'simpleoutput'      => true,
+    'packagedirectory'  => './',
+    'filelistgenerator' => 'file',
+    'ignore'            => array('phpunit-bootstrap.php', 'phpunit.xml', 'generatePackage.php'),
+    'dir_roles' => array(
+        'tests'       => 'test',
+        'public_html' => 'doc',
+        'doc'         => 'doc'
+    ),
+));
+
+$packagexml->setPackage('RandomString');
+$packagexml->setSummary('A RandomString generator');
+$packagexml->setDescription('Easy creation of random strings, limiting by lower case, upper case, numeric, and punctuation characters');
+
+$packagexml->setChannel('shupp.github.com/pirum');
+$packagexml->setAPIVersion('0.1.0');
+$packagexml->setReleaseVersion('0.1.0');
+
+$packagexml->setReleaseStability('alpha');
+
+$packagexml->setAPIStability('alpha');
+
+$packagexml->setNotes('
+* Initial release
+');
+$packagexml->setPackageType('php');
+$packagexml->addRelease();
+
+$packagexml->detectDependencies();
+
+$packagexml->addMaintainer('lead',
+                           'shupp',
+                           'Bill Shupp',
+                           'shupp@php.net');
+$packagexml->setLicense('New BSD License',
+                        'http://www.opensource.org/licenses/bsd-license.php');
+
+$packagexml->setPhpDep('5.1.2');
+$packagexml->setPearinstallerDep('1.4.0b1');
+
+$packagexml->generateContents();
+$packagexml->writePackageFile();
+
+?>
